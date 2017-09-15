80a90c4203332d50c349517598c43e45eb082718
Ldrumstick <liaochengtao@foxmail.com>
2017-09-15 13:08:25 +0800

    Begin

---
+++ huishizhi/Assets/Manager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum GameState
+{
+    MENU, //开始菜单
+    GAME, //游戏中
+    OVER  //游戏结束
+}
+
+public enum IsAttacker
+{
+    PLAYER,  //轮到玩家攻击
+    ENEMY   //轮到敌人攻击
+}
+
+
+/// <summary>
+/// 用于判断当前游戏状态
+/// </summary>
+public class Manager : MonoBehaviour {
+    public GameState gameState = GameState.MENU;
+    public IsAttacker isAttacker = IsAttacker.PLAYER;
+
+    public GameObject beginBt;
+    public GameObject attackBt;
+    public Player playerScript;
+    public Enemy enemyScript;
+
+    private void Start()
+    {
+        if(gameState == GameState.MENU)
+        {
+            beginBt.SetActive(true);
+        }
+        attackBt.SetActive(false);
+    }
+    /// <summary>
+    /// 当按下开始按钮
+    /// </summary>
+    public void ClickBeginBt()
+    {
+        gameState = GameState.GAME;
+        beginBt.SetActive(false);
+        playerScript.isShowingList = true;
+        attackBt.SetActive(true);
+    }
+}
+
+
---
+++ huishizhi/Assets/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public int playerHP = 100;
+    public bool isShowingList = false;
+    public GameObject attackBt;
+    public Manager manager;
+    public Enemy enemy;
+    public Animator animator;
+
+    private Transform initTransform;
+
+    private void Awake()
+    {
+        initTransform = transform;
+    }
+
+    void Update () {
+		if(manager.gameState == GameState.GAME)
+        {
+            if (manager.isAttacker == IsAttacker.PLAYER)
+            {
+                if (isShowingList == false)
+                {
+                    isShowingList = false;
+                    attackBt.SetActive(true);
+                }
+            }
+            else attackBt.SetActive(false);
+        }
+	}
+    /// <summary>
+    /// 当按下攻击按钮
+    /// </summary>
+    public void ClickAttackBt()
+    {
+        Debug.Log("玩家进行攻击");
+        isShowingList = false;
+        animator.SetTrigger("Attack");
+        manager.isAttacker = IsAttacker.ENEMY;
+        StartCoroutine(Wait());
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(1);
+        transform.position = initTransform.position;
+        transform.rotation = initTransform.rotation;
+    }
+}
